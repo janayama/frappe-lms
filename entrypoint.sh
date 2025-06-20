710ba73046e34805fcd55902eaff4b16bf399a7b
janayama <javier.anaya.martinez@gmail.com>
2025-06-20 11:31:05 +0200

    add site

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,9 @@ echo "--- [Frappe Entrypoint] Setting site configuration... ---"
 # Set common configuration using bench set-config
 
 mkdir -p "sites/$SITE_NAME"
-touch "sites/$SITE_NAME/site_config.json"
+cat <<EOF > "sites/$SITE_NAME/site_config.json"
+{}
+EOF
 
 bench --site "$SITE_NAME" set-config db_host "$MARIADB_HOST"
 bench --site "$SITE_NAME" set-config db_port "$MARIADB_PORT"
